9e2ca7d42eda3957e9a70933e4770820766ccde5
fpachilla <109562843+fpachilla@users.noreply.github.com>
2024-03-26 17:09:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ async function getCarta() {
         }
         const contenido = await response.text();
         const lineas = contenido.split('\n'); // Separar el contenido por líneas
-        //console.log('Contenido del archivo:', lineas);
 
         // Obtener un índice aleatorio dentro del rango de la longitud del array
         let indiceAleatorio = Math.floor(Math.random() * lineas.length);
@@ -23,6 +22,8 @@ async function getCarta() {
         console.log('Valor aleatorio 1:', valorAleatorio);
         document.getElementById("carta1").textContent = valorAleatorio;
 
+        indiceAleatorio = Math.floor(Math.random() * lineas.length);
+
         valorAleatorio = lineas[indiceAleatorio];
         lineas.splice(indiceAleatorio, 1)
         console.log(lineas)
@@ -30,10 +31,30 @@ async function getCarta() {
         console.log('Valor aleatorio 2:', valorAleatorio);
         document.getElementById("carta2").textContent = valorAleatorio;
 
+        indiceAleatorio = Math.floor(Math.random() * lineas.length);
+
+        valorAleatorio = lineas[indiceAleatorio];
+        lineas.splice(indiceAleatorio, 1)
+        console.log(lineas)
+
+        console.log('Valor aleatorio 3:', valorAleatorio);
+        document.getElementById("carta3").textContent = valorAleatorio;
+
+        // Obtener todos los elementos con la clase "carta"
+        var cartas = document.querySelectorAll(".carta");
+
+        // Iterar sobre cada elemento y agregar la clase "mostrar"
+        cartas.forEach(function(carta) {
+            carta.classList.add("mostrar");
+        });
+
+        // Esperar un breve momento para permitir que las cartas se muestren completamente
+        setTimeout(function() {
+            // Agregar la clase 'blur' al body para aplicar el efecto de desenfoque
+            document.getElementById("container").className = "difuminado";
+        }, 1000); // Ajusta el tiempo según sea necesario
+
     } catch (error) {
         console.error('Error:', error);
     }
-}
-
-function sorteo(miArray){
 }
