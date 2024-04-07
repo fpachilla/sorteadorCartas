7c3ae88e8d7f49125b3a79bd8eac4d4aa19bd35d
fpachilla <109562843+fpachilla@users.noreply.github.com>
2024-04-07 00:00:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Ruta del archivo txt predefinido
 const rutaArchivo = 'cartas.txt';
 
+var cartas = [];
+
 // Función para leer el archivo y guardar las líneas en un array
 async function getCarta() {
     try {
@@ -9,33 +11,33 @@ async function getCarta() {
             throw new Error('Error al leer el archivo');
         }
         const contenido = await response.text();
-        const lineas = contenido.split('\n'); // Separar el contenido por líneas
+        cartas = contenido.split('\n'); // Separar el contenido por líneas
 
-        // Obtener un índice aleatorio dentro del rango de la longitud del array
-        let indiceAleatorio = Math.floor(Math.random() * lineas.length);
+        /// Obtener un índice aleatorio dentro del rango de la longitud del array
+        // let indiceAleatorio = Math.floor(Math.random() * cartas.length);
 
-        // Obtener el valor aleatorio utilizando el índice aleatorio generado
-        let valorAleatorio = lineas[indiceAleatorio];
-        lineas.splice(indiceAleatorio, 1)
-        console.log(lineas)
+        /// Obtener el valor aleatorio utilizando el índice aleatorio generado
+        // let valorAleatorio = cartas[indiceAleatorio];
+        // cartas.splice(indiceAleatorio, 1)
+        // console.log(cartas)
 
-        console.log('Valor aleatorio 1:', valorAleatorio);
-        document.getElementById("carta1").textContent = valorAleatorio;
+        // console.log('Valor aleatorio 1:', valorAleatorio);
+        document.getElementById("carta1").textContent = getValorRandom();
 
-        indiceAleatorio = Math.floor(Math.random() * lineas.length);
+        indiceAleatorio = Math.floor(Math.random() * cartas.length);
 
-        valorAleatorio = lineas[indiceAleatorio];
-        lineas.splice(indiceAleatorio, 1)
-        console.log(lineas)
+        valorAleatorio = cartas[indiceAleatorio];
+        cartas.splice(indiceAleatorio, 1)
+        console.log(cartas)
 
         console.log('Valor aleatorio 2:', valorAleatorio);
         document.getElementById("carta2").textContent = valorAleatorio;
 
-        indiceAleatorio = Math.floor(Math.random() * lineas.length);
+        indiceAleatorio = Math.floor(Math.random() * cartas.length);
 
-        valorAleatorio = lineas[indiceAleatorio];
-        lineas.splice(indiceAleatorio, 1)
-        console.log(lineas)
+        valorAleatorio = cartas[indiceAleatorio];
+        cartas.splice(indiceAleatorio, 1)
+        console.log(cartas)
 
         console.log('Valor aleatorio 3:', valorAleatorio);
         document.getElementById("carta3").textContent = valorAleatorio;
@@ -51,10 +53,25 @@ async function getCarta() {
         // Esperar un breve momento para permitir que las cartas se muestren completamente
         setTimeout(function() {
             // Agregar la clase 'blur' al body para aplicar el efecto de desenfoque
-            document.getElementById("container").className = "difuminado";
-        }, 1000); // Ajusta el tiempo según sea necesario
+            //document.getElementById("container").className = "difuminado";
+        }, 10); // Ajusta el tiempo según sea necesario
 
     } catch (error) {
         console.error('Error:', error);
     }
+}
+
+function getValorRandom(){
+    // Obtener un índice aleatorio dentro del rango de la longitud del array
+    let indiceAleatorio = Math.floor(Math.random() * cartas.length);
+
+    // Obtener el valor aleatorio utilizando el índice aleatorio generado
+    let valorAleatorio = cartas[indiceAleatorio];
+    cartas.splice(indiceAleatorio, 1)
+    console.log(cartas)
+
+    // Muestra en pantalla el valor obtenido
+    console.log('Valor aleatorio:', valorAleatorio);
+
+    return valorAleatorio;
 }
